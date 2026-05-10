6e34065795b8c5dd2c81bf95500c8a08828e8fc3
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2026-05-10 15:51:10 -0300

    Correção Final da parte do seed e Prisma 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "prisma/seed.ts",
+    seed: "ts-node prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../src/generated/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 // initialize Prisma Client

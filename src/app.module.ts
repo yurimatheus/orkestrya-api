dc52b41b4965ccb9f722b5c75eb39e72a3106095
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2026-05-10 16:28:32 -0300

    - Pasta "articles" adicionada - Adição da estrutura base

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AgentsModule } from './agents/agents.module';
 import { ChatModule } from './chat/chat.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
     AgentsModule,
     ChatModule,
     PrismaModule,
+    ArticlesModule,
   ],
 })
 export class AppModule {}

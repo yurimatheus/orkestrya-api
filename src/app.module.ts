c7761da73c41b49c32f96387b91b7e1587e89827
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2026-05-10 16:13:13 -0300

    Serviço do Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AgentsModule } from './agents/agents.module';
 import { ChatModule } from './chat/chat.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     AgentsModule,
     ChatModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}

1d6bdf2a6597ec37e0618e90d114cff1bc2016d8
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2026-05-10 16:16:21 -0300

    Adição do titulo, descrição, versão e build do DocBuilder do Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 
@@ -13,6 +14,15 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.enableCors();
+  
+  const config = new DocumentBuilder()
+    .setTitle('Orkestrya API')
+    .setDescription('API for managing agents')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
   console.log('🚀 Agent API rodando em http://localhost:3000');
   console.log('📋 Agents disponíveis: GET  http://localhost:3000/agents');

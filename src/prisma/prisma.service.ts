42fca7fe1577ded3980553e0861a6d2e9f5d22ce
Yuri Matheus Gomes <y.matheusdesign@gmail.com>
2026-05-10 16:27:57 -0300

    Atualizzação da estrutura do prisma 7

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,25 @@
-import { INestApplication, Injectable } from '@nestjs/common';
-import { PrismaClient } from '../../src/generated/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
